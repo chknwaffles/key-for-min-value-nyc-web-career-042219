e062ac6480a1a3df863331eb265236bc8f1d0587
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:27:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  result = ""
-  smallest = 0
-  next_smallest = 0
+  name = ""
+  value = ""
   name_hash.collect do |k, v|
-    next_smallest = v
-    if smallest < next_smallest
-      smallest = next_smallest
-      result = k
-    else
-      smallest = next_smallest
-      result = k
-    end
+    
   end
   result
 end
